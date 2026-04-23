df7a655521ce4133658e42967746d12c91d1ad22
Jazz <106460341+jazxbx@users.noreply.github.com>
2026-04-23 06:09:59 -0700

    feat(auth): add refresh and logout routes -moved refresh token to httpOnly cookie and return access token only -added cookie-parser package

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  refreshAccessToken,
+} from '../controllers/auth.controller.js';
 import { loginSchema, registerSchema } from '../validators/auth.validator.js';
 import { validateRequest } from '../middleware/validateRequest.middleware.js';
 
@@ -7,4 +12,7 @@ const router = express.Router();
 
 router.post('/register', validateRequest(registerSchema), registerUser);
 router.post('/login', validateRequest(loginSchema), loginUser);
+router.post('/refresh', refreshAccessToken);
+router.post('/logout', logoutUser);
+
 export default router;
