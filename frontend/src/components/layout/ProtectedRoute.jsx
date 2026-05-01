28dac8d3397f5fd93d358bd8ffe0be841c885bba
Jazz <106460341+jazxbx@users.noreply.github.com>
2026-05-01 00:25:52 -0700

    refactor: add import....

--- frontend/src/components/layout/ProtectedRoute.jsx
+++ frontend/src/components/layout/ProtectedRoute.jsx
@@ -1,8 +1,10 @@
 import { useAuth } from '@/features/auth';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return null;
 
   return isAuthenticated ? <Outlet /> : <Navigate to='/login' replace />;
 }
